1638ff0ae199317427b54d695b5c3f590f9b8ca0
Rekxis <Rekxis@gmx.de>
2023-07-13 11:01:38 +0200

    03 Deklaration + Wertzuweisung II

--- script_01.js
+++ script_01.js
@@ -15,3 +15,33 @@
 
 /***** 02 Deklaration + Wertzuweisung I *******/
 
+// let firstName; // Deklaration (Definition)
+// firstName = "Max"; //Wertzuweisung
+// console.log(firstName); // Ausgabe 
+
+// let familyName;
+// familyName = "Muetze";
+// console.log(familyName);
+
+/***** 03 Deklaration + Wertzuweisung II *******/
+
+// let firstName, familyName;
+
+// firstName = prompt("Bitte Vornamen eingeben:");
+// familyName = prompt("Bitte Nachnamen eingeben:");
+
+// console.log(firstName + " " + familyName);
+// console.log(firstName, familyName);
+// console.log(`${firstName} ${familyName}`); // Template literals
+
+let test;
+test = "Maxine";
+test = 2;
+test = false;
+test = "2";
+
+console.log("Datentyp: " + typeof test);
+console.log("Inhalt: " + test);
+
+console.log("11" + 1);  // 111
+console.log("11" - 1);  // 10
